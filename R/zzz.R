425b7f17d5b01370f1eb254889d96d224ed2b22d
Xin Chen <chenx26@uw.edu>
2017-08-26 14:17:28 -0600

    unsucessful attemp to use Roxygen on RcppEigen package. Error message: current directory must be a package

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+#' glmnetRcpp
+#'
+#' Efficient Implementation of glmnet using Rcpp and RcppEigen
+#'
+#' @docType package
+#' @author Xin Chen <chenx26@uw.edu>
+#' @import Rcpp RcppEigen
+#' @importFrom Rcpp evalCpp
+#' @useDynLib glmnetRcpp
+#' @name glmnetRcpp
+NULL
